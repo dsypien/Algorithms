c24d354d6943e85fc4918f32ebb3f9d582c537fc
dsypien <davidsypien@gmail.com>
2013-08-10 23:43:24 -0400

    Algorithms repository check in.  Insertion sort complete

---
+++ InsertionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Algorithms.Sort
+{
+    public static class InsertionSort
+    {
+        public static List<int> Sort (IEnumerable<int> collection)
+        {
+            List<int> list = collection.ToList();
+
+            for (int i = 1; i < list.Count ; i++)
+            {
+                int key = list[i];
+
+                for (int j = i -1 ; j >= 0 && key < list[j]  ; j--)
+                {
+                    int temp = list[j];
+                    list[j] = key;
+                    list[j + 1] = temp;
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Algorithms.Sort;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> unsortedList = GetUnsortedList();
+
+            var insertionSortResult = InsertionSort.Sort(unsortedList);
+            Console.WriteLine("blah");
+        }
+
+        static private List<int> GetUnsortedList(int length = 10)
+        {
+            List<int> list = new List<int>();
+
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                list.Add(random.Next(0,250));
+            }
+
+            return list;
+        }
+    }
+}
